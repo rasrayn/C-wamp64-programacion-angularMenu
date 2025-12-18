09b6e87b78c65295838d4def3b57069698fb62fd
Ramses <rasrayn1@gmail.com>
2025-12-18 12:38:46 +0100

    Aplicacion web para pedir comida, creacion de la navegacion y los componentes del menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,35 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { Carrito } from './pages/carrito/carrito';
+import { Articulo } from './pages/articulo/articulo';
+import { Buscar } from './pages/buscar/buscar';
+import { Menu } from './pages/menu/menu';
+import { Perfil } from './pages/perfil/perfil';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: Home
+    },
+    {
+        path: "carrito",
+        component: Carrito
+    },
+    {
+        path: "articulo",
+        component: Articulo
+    },
+    {
+        path: "buscar",
+        component: Buscar
+    },
+    {
+        path: "menu",
+        component: Menu
+    },
+    {
+        path: "perfil",
+        component: Perfil
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Tabs } from './components/tabs/tabs';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, Tabs],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('angularMenu');
